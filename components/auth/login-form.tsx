9f0189ad2e8e6f846677ea70bef19c5ed3b2c7fa
Kane Chang <kane.chang.fl@gmail.com>
2024-07-14 11:40:38 +0100

    added login schema and login form components

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,10 +1,59 @@
-import { CardWrapper } from "./card-wrapper"
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { LoginSchema } from "@/schemas";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { CardWrapper } from "@/components/auth/card-wrapper";
+import { Input } from "@/components/ui/input";
 
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
   return (
-    <CardWrapper headerLabel="Welcome Back" backButtonLabel="Don't have an account?" backButtonHref="/register" showSocial>
-      Login Form
+    <CardWrapper
+      headerLabel="Welcome Back"
+      backButtonLabel="Don't have an account?"
+      backButtonHref="/register"
+      showSocial
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="john.doe@example.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage/>
+                </FormItem>
+              )}
+            />
+          </div>
+        </form>
+      </Form>
     </CardWrapper>
-  )
-}
+  );
+};
