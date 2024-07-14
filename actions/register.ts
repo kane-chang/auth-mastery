4acffcbcfb93a71e9a222aed6393df9f044f031c
Kane Chang <kane.chang.fl@gmail.com>
2024-07-14 14:10:12 +0100

    added register functionality to create users to db

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 "use server";
 
+import { getUserByEmail } from "@/data/user";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt"
 import * as z from "zod";
 
 export const register =  async (values: z.infer<typeof RegisterSchema>) => {
@@ -9,6 +12,27 @@ export const register =  async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { error: "Invalid fields!"};
   }
+
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) {
+    return {
+      error: "Email already in use!"
+    }
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword
+    }
+  })
+
+  // TODO: send verification token
 
   return { success: "Email verification sent!"}
 };
